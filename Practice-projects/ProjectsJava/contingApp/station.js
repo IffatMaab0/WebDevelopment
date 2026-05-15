b46af38ff6409d4604c06bc3815d6fe277efacac
IffatMaab0 <iffatmaab11@gmail.com>
2026-05-15 15:20:24 +0500

    project completed

--- Practice-projects/ProjectsJava/contingApp/station.js
+++ Practice-projects/ProjectsJava/contingApp/station.js
@@ -1,10 +1,15 @@
 let countEl = document.getElementById("count");
+let preVious = document.getElementById("previous")
 console.log(countEl);
 let count = 0;
 function increment(){
     count +=1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 function save(){
-    console.log(count)
+    let saved = count + " - ";
+    preVious.textContent +=  saved;
+    console.log(saved);
+    count =0;
+    countEl.innerText = count;
 }
--- first.js
+++ first.js
@@ -1,1 +1,30 @@
+let num1 = 8;
+let num2 = 2;
+let result = document.getElementById("res");
+
+let num1El = document.getElementById("num1-el");
+let num2El = document.getElementById("num2-el");
+num1El.textContent = num1;
+num2El.textContent = num2;
+
+function add(){
+    result.textContent = "Result:" + " "
+    let sum = num1 + num2;
+    result.textContent = result.textContent + sum;
+}
+function subtract(){
+    result.textContent = "Result:" + " "
+    let sub = num1- num2;
+    result.textContent = result.textContent + sub;
+}
+function multiply(){
+    result.textContent = "Result:" + " "
+    let product = num1 * num2;
+    result.textContent = result.textContent + product;
+}
+function divide(){
+    result.textContent = "Result:" + " "
+    let division = num1/num2;
+    result.textContent = result.textContent + division;
+}
 
