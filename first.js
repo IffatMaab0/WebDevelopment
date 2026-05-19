d4f4c751e85d10a0b716422cf28eb7ee7f802f70
IffatMaab0 <iffatmaab11@gmail.com>
2026-05-19 10:31:51 +0500

    advanced concepts practice

---
+++ first.js
@@ -0,0 +1,9 @@
+const dreamHoliday = {
+    destination : "Egypt",
+    activity : "Explore the ancient building",
+    accomodation : "in a smal apartment",
+    companion : "Alone"
+};
+
+const {destination , activity, accomodation, companion} = dreamHoliday;
+console.log(`i would love to visit ${destination},  ${activity} and ${accomodation} ${companion}`)
